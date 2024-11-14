078bfa7f6c90ed31055f4eaef72fdd1d09a09330
Fatih Nar <fenar@yahoo.com>
2024-11-14 15:31:46 -0600

    Update starlink-inference.py

--- starlink/starlink-inference.py
+++ starlink/starlink-inference.py
@@ -75,4 +75,4 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=35001, debug=True)
+    app.run(host='0.0.0.0', port=35001, debug=False)
